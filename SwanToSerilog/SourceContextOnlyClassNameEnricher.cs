2f14d2a851691ca08df4f69a8e38ca24664040fd
asveredyuk <alexsv1802@gmail.com>
2023-05-04 02:38:59 +0600

    Added classes

---
+++ SwanToSerilog/SourceContextOnlyClassNameEnricher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace SwanToSerilog
+{
+    public class SourceContextOnlyClassNameEnricher : ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            if (logEvent.Properties.TryGetValue("SourceContext", out var property))
+            {
+                var scalarValue = property as ScalarValue;
+                var value = scalarValue?.Value as string;
+                if (value == null)
+                {
+                    return;
+                }
+                var lastElement = value.Split('.').LastOrDefault();
+                if (!string.IsNullOrWhiteSpace(lastElement))
+                {
+                    logEvent.AddOrUpdateProperty(new LogEventProperty("SourceContext", new ScalarValue(lastElement)));
+                }
+
+            }
+        }
+    }
+}
---
+++ SwanToSerilog/SwanToSerilogLogger.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+using Serilog.Events;
+using Serilog.Parsing;
+
+namespace SwanToSerilog
+{
+    /// <summary>
+    /// Transfers log messages from swan logger to serilog
+    /// </summary>
+    public class SwanToSerilogLogger : Swan.Logging.ILogger
+    {
+        private readonly ILogger _log;
+
+        public SwanToSerilogLogger(ILogger log)
+        {
+            _log = log;
+        }
+
+        public void Dispose()
+        {
+            //do nothing, no disposable parts
+        }
+
+        public void Log(Swan.Logging.LogMessageReceivedEventArgs logEvent)
+        {
+            var sourceName = logEvent.Source;
+            var logLevel = ConvertLogLevel(logEvent.MessageType);
+            var exception = logEvent.Exception;
+            var msg = logEvent.Message;
+            var log = _log;
+            if (!string.IsNullOrWhiteSpace(sourceName))
+            {
+                log = _log.ForContext("SourceContext", sourceName);
+            }
+            log.Write(logLevel, exception, msg);
+        }
+
+        public static LogEventLevel ConvertLogLevel(Swan.Logging.LogLevel level)
+        {
+            var num = (int)level;
+            return (LogEventLevel)Math.Max(num - 1, 0);
+        }
+
+        public Swan.Logging.LogLevel LogLevel { get; set; }
+    }
+}
